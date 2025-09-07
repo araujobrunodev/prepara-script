b652e577c93b98a7622a45af0b39bb8bba636b6c
araujobrunodev <ballogggamer@gmail.com>
2025-09-07 11:25:50 -0300

    Add: implement askRa and askPassword to login

--- src/CLI/paths/login.js
+++ src/CLI/paths/login.js
@@ -1,11 +1,13 @@
-
+const askRA = require("../features/askRA")
+const askPassword = require("../features/askPassword")
 
 async function login () {
     console.log("\n======================")
     console.log("   login page   ")
     console.log("======================\n")
-    //ask email
-    //ask password
+    
+    const ra = askRA()
+    const password = askPassword()
 }
 
 module.exports = login
